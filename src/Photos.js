d267c50fc44b3c9c8f261669d1ea139380665cd0
valeria-markish <84080359+valeria-markish@users.noreply.github.com>
2023-11-15 19:05:28 +0100

    Finished code to display 9 photos

--- src/Photos.js
+++ src/Photos.js
@@ -4,33 +4,28 @@ import axios from "axios";
 export default function Photos(props) {
   const [dataPhotos, setDataPhotos] = useState(null);
   function handlePhotos(response) {
-    // console.log(response.data);
     setDataPhotos(response.data.photos);
   }
   let apiKeyPexels = "KxHdtbLuNaHyq8ZwdGUUsK9HhImpDlD51oXOwcYIy9GJt3P7HoaIEQkv";
   let headers = { Authorization: `${apiKeyPexels}` };
-  let apiUrlPexels = `https://api.pexels.com/v1/search?query=${props.word}&per_page=1`;
+  let apiUrlPexels = `https://api.pexels.com/v1/search?query=${props.word}&per_page=9`;
   axios.get(apiUrlPexels, { headers: headers }).then(handlePhotos);
 
   if (dataPhotos) {
     return (
-      <div className="section">
+      <div className="section photo-gallery">
         {dataPhotos.map(function (showPhotos, index) {
-          if (index < 6) {
-            return (
-              <div key={index}>
-                <a
-                  href={showPhotos.src.original}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img src={showPhotos.src.tiny} alt={showPhotos.alt} />
-                </a>
-              </div>
-            );
-          } else {
-            return null;
-          }
+          return (
+            <div key={index}>
+              <a
+                href={showPhotos.src.original}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={showPhotos.src.tiny} alt={showPhotos.alt} />
+              </a>
+            </div>
+          );
         })}
       </div>
     );
